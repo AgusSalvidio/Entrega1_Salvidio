4d54f7d355a2c9a7bed25e1aaa01afcb25741c41
Alan Schanzenbach <alanschanzenbach@gmail.com>
2022-09-05 22:18:42 -0300

    added forms for promo code and player cards

--- Qatar_World_Cup_2022/AppQatarWC2022/views.py
+++ Qatar_World_Cup_2022/AppQatarWC2022/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import *
 from .models import *
 from django.http import HttpResponse
 
@@ -39,3 +39,30 @@ def user_registration(request):
         return render(request,"users.html",context)
     else:  
         return render(request,"user_registration.html")
+
+#-------------------------------------------------------------
+def player_card_registration(request):
+    if request.method=="POST":
+        first_name = request.POST["card_first_name"]
+        last_name = request.POST["card_last_name"]
+        country = request.POST["card_country"]
+        birthdate = request.POST["card_birthdate"]
+        position = request.POST["card_position"]
+        card=PlayerCard(first_name = first_name,last_name = last_name,birthdate = birthdate,country = country,position=position)
+        card.save()
+        context={"cards": PlayerCard.objects.all()}
+        return render(request, "player_cards.html", context)
+    else:
+        return render(request, "player_card_registration.html")
+
+#------------------------------------------------
+def promo_codes_registration(request):
+    if request.method=="POST":
+        code=request.POST["code"]
+        promo_code=PromoCode(code=code)
+        promo_code.save()
+        context={"code": PromoCode.objects.all()}
+        return render(request, "promo_codes.html", context)
+    else:
+        return render(request, "promo_codes_registration.html")
+    
