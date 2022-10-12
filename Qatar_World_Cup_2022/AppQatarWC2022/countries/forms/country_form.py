be969eda4a9e4879216a81ae470bfbd6a1a76be8
Agustin Salvidio <agustinsalvidio@gmail.com>
2022-10-11 22:19:30 -0300

    :bug: Fixed some bugs

--- Qatar_World_Cup_2022/AppQatarWC2022/countries/forms/country_form.py
+++ Qatar_World_Cup_2022/AppQatarWC2022/countries/forms/country_form.py
@@ -2,7 +2,7 @@ from django import forms
 
 class CountryRegistration(forms.Form):
     full_name = forms.CharField(label='Nombre',max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
-    qualified = forms.BooleanField(label='Clasificado',widget=forms.CheckboxInput(attrs={'class':'form-control'}),initial=False)
+    qualified = forms.BooleanField(label='Clasificado',widget=forms.CheckboxInput(attrs={'class':'form-control'}),initial=False,required=False)
     background_image = forms.ImageField(label='Fondo',widget=forms.FileInput(attrs={'class':'form-control'}),required=False)
 
     def name(self):
