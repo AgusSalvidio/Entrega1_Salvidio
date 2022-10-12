908c0c7f137444b77559932ab5c13b4c2c304246
Agustin Salvidio <agustinsalvidio@gmail.com>
2022-10-11 21:51:33 -0300

    :bug: Fixed a bug when reloading page when opening packs

--- Qatar_World_Cup_2022/AppQatarWC2022/stickers/views/open_pack_view.py
+++ Qatar_World_Cup_2022/AppQatarWC2022/stickers/views/open_pack_view.py
@@ -7,4 +7,4 @@ working_context = {'working_context':app.working_context()}
 @login_required
 def open_pack(request):
     sticker_pack = app.working_context().sticker_pack_for(app.working_context().logged_user())
-    return render(request,"open_pack.html",{'sticker_pack':sticker_pack}) 
+    return render(request,"open_pack.html",{'sticker_pack':sticker_pack,'working_context':app.working_context()}) 
